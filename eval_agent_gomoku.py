7f3765b8835aa57a6469c89bc15ea85e9f5c103d
Hanlin_Tan <hanlin_tan@nudt.edu.cn>
2024-02-03 14:59:32 +0800

    update eval params to fit 15x15 gomuku

--- eval_agent_gomoku.py
+++ eval_agent_gomoku.py
@@ -11,28 +11,30 @@ import sys
 import torch
 
 FLAGS = flags.FLAGS
-flags.DEFINE_integer('board_size', 13, 'Board size for freestyle Gomoku.')
+flags.DEFINE_integer('board_size', 15, 'Board size for freestyle Gomoku.')
 flags.DEFINE_integer(
     'num_stack',
-    8,
+     8,
     'Stack N previous states, the state is an image of N x 2 + 1 binary planes.',
 )
 flags.DEFINE_integer('num_res_blocks', 10, 'Number of residual blocks in the neural network.')
-flags.DEFINE_integer('num_filters', 40, 'Number of filters for the conv2d layers in the neural network.')
+flags.DEFINE_integer('num_filters', 64, 'Number of filters for the conv2d layers in the neural network.')
 flags.DEFINE_integer(
     'num_fc_units',
-    80,
+     96,
     'Number of hidden units in the linear layer of the neural network.',
 )
 
 flags.DEFINE_string(
     'black_ckpt',
-    './checkpoints/gomoku/13x13/training_steps_170000.ckpt',
+    # './checkpoints/gomoku/13x13/training_steps_170000.ckpt',
+    './checkpoints/gomoku/15x15/training_steps_25000.ckpt',
     'Load the checkpoint file for black player.',
 )
 flags.DEFINE_string(
     'white_ckpt',
-    './checkpoints/gomoku/13x13/training_steps_200000.ckpt',
+    # './checkpoints/gomoku/13x13/training_steps_200000.ckpt',
+    './checkpoints/gomoku/15x15/training_steps_35000.ckpt',
     'Load the checkpoint file for white player.',
 )
 
--- eval_agent_gomoku_cmd.py
+++ eval_agent_gomoku_cmd.py
@@ -12,7 +12,7 @@ import sys
 import torch
 
 FLAGS = flags.FLAGS
-flags.DEFINE_integer('board_size', 13, 'Board size for freestyle Gomoku.')
+flags.DEFINE_integer('board_size', 15, 'Board size for freestyle Gomoku.') # old: 13
 flags.DEFINE_integer(
     'num_stack',
     8,
@@ -28,12 +28,14 @@ flags.DEFINE_integer(
 
 flags.DEFINE_string(
     'black_ckpt',
-    './checkpoints/gomoku/13x13/training_steps_170000.ckpt',
+    #'./checkpoints/gomoku/13x13/training_steps_170000.ckpt',
+    './checkpoints/gomoku/15x15/ training_steps_26000.ckpt',
     'Load the checkpoint file for black player.',
 )
 flags.DEFINE_string(
     'white_ckpt',
-    './checkpoints/gomoku/13x13/training_steps_200000.ckpt',
+    # './checkpoints/gomoku/13x13/training_steps_200000.ckpt',
+    './checkpoints/gomoku/15x15/ training_steps_26000.ckpt',
     'Load the checkpoint file for white player.',
 )
 
